ffc1a719ad1b768c3cdd0da8bc20afb47d635fe2
dande-vivian <118072739+viviandande@users.noreply.github.com>
2022-12-16 23:50:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,55 +1,62 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    fetchFirstMovieContent()
-    fetchAllMoviesContent()
-
-})
-
-function fetchFirstMovieContent(){
-    return fetch("http://localhost:3000/films/1")
-    .then(resp => resp.json())
-    .then(data => firstMovieContent(data))
-}
-
-
-function firstMovieContent(data){
-    const poster = document.getElementById("poster");
-    poster.src = data.poster;
-    const title = document.getElementById("title");
-    title.textContent = data.title;
-    const runtime = document.getElementById("runtime");
-    runtime.textContent = data.runtime;
-    const showtime = document.getElementById("showtime");
-    showtime.textContent = data.showtime;
-    const description = document.getElementById("film-info");
-    description.textContent = data.description;
-    const remainingTickets = document.getElementById("ticket-num")
-    remainingTickets.textContent = data.capacity - data.tickets_sold;
-    const button =document.getElementById("buy-ticket");
-    button.addEventListener("click", (event) =>{
-        if(remainingTickets.textContent > 0){
-            remainingTickets.textContent -= 1
-        if(remainingTickets.textContent == 0){
-            button.textContent = "sold out";
-            button.classList.remove("orange")
-        }}
-    })
-
-}
-function fetchAllMoviesContent(data){
-    return fetch("http://localhost:3000/films")
-    .then(resp => resp.json())
-    .then(data =>data.forEach(data => fetchAllMoviesList(data)))
-
-}
-function fetchAllMoviesList(data){
-    const filmSection = document.getElementById("films");
-    const filmList = document.createElement("li");
-    filmList.setAttribute("class", "film item");
+function renderAllMovies(data){
+    const filmSection = document.querySelector('#films');
+    const filmList = document.createElement('li');
+    filmList.setAttribute('class', 'film item');
     filmList.textContent = data.title;
     filmSection.appendChild(filmList);
-    document.addEventListener("click", () =>{
-        let title = document.getElementById("title");
-        title.textContent = data.title
+    filmList.addEventListener('click',()=>{
+      firstMovieDetails(data)
     })
+  }
 
-}
+
+  function firstMovieDetails(data){
+    // console.log(data)
+    const poster = document.querySelector('div.four.wide.column img#poster');
+    poster.src = data.poster;
+
+    // title
+    const title = document.querySelector('#title')
+    title.textContent = data.title;
+
+    // runtime
+    const runtime = document.querySelector('#runtime')
+    runtime.textContent = data.runtime;
+
+    // descriptiona
+    const description = document.querySelector('#film-info')
+    description.textContent = data.description;
+
+    // showtime
+    const showtime = document.querySelector('#showtime')
+    showtime.textContent = data.showtime
+
+    // remainder tickets
+    const remainingTickets = document.querySelector('#ticket-num')
+    remainingTickets.textContent = data.capacity - data.tickets_sold
+   const button =document.getElementById("buy-ticket");
+      button.addEventListener("click", (event) =>{
+          if(remainingTickets.textContent > 0){
+              remainingTickets.textContent -= 1
+          if(remainingTickets.textContent == 0){
+              button.textContent = "sold out";
+              button.classList.remove("orange")
+          }}
+
+  })}
+
+  function fetchFirstMovieData(){
+    return fetch('http://localhost:3000/films/1')
+    .then(res => res.json())
+    .then(data => firstMovieDetails(data))
+  }
+  function fetchAllMovies(){
+    return fetch('http://localhost:3000/films')
+    .then(res => res.json())
+    .then(data => data.forEach(item => renderAllMovies(item)))
+  }
+
+  document.addEventListener('DOMContentLoaded',() => {
+    fetchFirstMovieData()
+    fetchAllMovies()
+  })
